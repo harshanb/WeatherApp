1bb415482cbeded3dbcc9f1e74e92b8e8e87a98c
Harshan Balasubramanian <584294@TCS.com>
2019-01-08 15:36:29 +0530

    Creating the weather-app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherAppComponent } from './weather-app/weather-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/weather-app/weather-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-app',
+  templateUrl: './weather-app.component.html',
+  styleUrls: ['./weather-app.component.css']
+})
+export class WeatherAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
