8a3d0acf09e0fa03a30cf2ebc8d6e80847d03e01
Harshan Balasubramanian <584294@TCS.com>
2019-01-08 15:38:05 +0530

    Created the displaydetails component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherAppComponent } from './weather-app/weather-app.component';
+import { DisplaydetailsComponent } from './displaydetails/displaydetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherAppComponent
+    WeatherAppComponent,
+    DisplaydetailsComponent
   ],
   imports: [
     BrowserModule,
